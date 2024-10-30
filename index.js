a9f626850f902a42ac753929320e07fdaf2fee24
awakra <lucasadls0@gmail.com>
2024-10-29 23:44:02 -0300

    Update index.js
    Changed all the code to English and optmized the if/else structure.

--- index.js
+++ index.js
@@ -1,46 +1,29 @@
 let test = 0;
 while (test <= 110) {
   rankCalc(test);
-  console.log(`Esta verificação foi feita com o valor de wins = ${test}`);
+  console.log(`This check was made with wins = ${test}`);
   test += 10;
 }
+
 function rankCalc(wins, losses = 0) {
-  let balance = wins - losses;
+  const balance = wins - losses;
   let rank;
+
   if (balance <= 10) {
-    rank = "Ferro";
-    return console.log(
-      `O Herói tem de saldo de ${balance} está no nível de ${rank}`
-    );
-  } else if (balance >= 11 && balance <= 20) {
+    rank = "Iron";
+  } else if (balance <= 20) {
     rank = "Bronze";
-    return console.log(
-      `O Herói tem de saldo de ${balance} está no nível de ${rank}`
-    );
-  } else if (balance >= 21 && balance <= 50) {
-    rank = "Prata";
-    return console.log(
-      `O Herói tem de saldo de ${balance} está no nível de ${rank}`
-    );
-  } else if (balance >= 51 && balance <= 80) {
-    rank = "Ouro";
-    return console.log(
-      `O Herói tem de saldo de ${balance} está no nível de ${rank}`
-    );
-  } else if (balance >= 81 && balance <= 90) {
-    rank = "Diamante";
-    return console.log(
-      `O Herói tem de saldo de ${balance} está no nível de ${rank}`
-    );
-  } else if (balance >= 91 && balance <= 100) {
-    rank = "Lendário";
-    return console.log(
-      `O Herói tem de saldo de ${balance} está no nível de ${rank}`
-    );
+  } else if (balance <= 50) {
+    rank = "Silver";
+  } else if (balance <= 80) {
+    rank = "Gold";
+  } else if (balance <= 90) {
+    rank = "Diamond";
+  } else if (balance <= 100) {
+    rank = "Legendary";
   } else {
-    rank = "Imortal";
-    return console.log(
-      `O Herói tem de saldo de ${balance} está no nível de ${rank}`
-    );
+    rank = "Immortal";
   }
+
+  console.log(`The Hero with a balance of ${balance} is at the ${rank} level`);
 }
